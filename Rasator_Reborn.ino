b87ea586d6617c610eb2ae499d15205ba0dd4103
Ömer Talha Başoğlu <edriyin@gmail.com>
2023-02-06 18:06:23 +0300

    LCD connection changed from Hardware SPI to Software SPI and Timer Interrupt added for counting a minute.

--- Rasator_Reborn.ino
+++ Rasator_Reborn.ino
@@ -9,17 +9,33 @@
 #define display_CS 10
 #define display_RST 8
 #define display_LED 5
-
+#define display_MOSI 6
+#define display_SCK 4
 #define button 3
 
-volatile bool isPressed{ false };
-volatile unsigned long last_interrupt_time{ 0 };
+volatile bool ledState{ false };                // Backlight led state of LCD
+volatile unsigned long lastInterruptTime{ 0 };  // Button debounce
+
 Adafruit_BME280 bme280;
-Adafruit_PCD8544 display{ display_DC, display_CS, display_RST };
+Adafruit_PCD8544 display{ display_SCK, display_MOSI, display_DC, display_CS, display_RST };
+
+volatile unsigned long counter{ 0 };
 
 void setup() {
 
-  pinMode(3, INPUT);
+  pinMode(3, INPUT);  // Button input
+
+  //Timer Interrupt setup
+  noInterrupts();
+  TCCR1A = 0;           //Reset entire TCCR1A register
+  TCCR1B = 0;           //Reset entire TCCR1B register
+  TCCR1B |= B00000101;  //Set CS12 to 1 so we get Prescalar = 1024
+  TIMSK1 |= B00000010;  //Set OCIE1A to 1 so we enable compare match A
+  OCR1A = 65535;        //Finally we set compare register A to this value
+  interrupts();
+
+  attachInterrupt(digitalPinToInterrupt(button), button_press, CHANGE);  // Button interrupt for backlight of LCD
+
   bme280.begin();
   bme280.setSampling(Adafruit_BME280::MODE_FORCED,  // single-read
                      Adafruit_BME280::SAMPLING_X1,  // temperature
@@ -27,30 +43,35 @@ void setup() {
                      Adafruit_BME280::SAMPLING_X1,  // humidity
                      Adafruit_BME280::FILTER_OFF);
   display.begin();
-  attachInterrupt(digitalPinToInterrupt(button), button_press, CHANGE);
 }
 
 void loop() {
 
   bme280.takeForcedMeasurement();  // has no effect in normal mode
   printValues();
-  delay(60000);
+  //delay(60000);
+}
+
+ISR(TIMER1_COMPA_vect) {
+
+  TCNT1 = 0;  //First, set the timer back to 0 so it resets for next interrupt
+  counter++;
 }
 
 void button_press() {
 
-  const unsigned long interrupt_time = millis();
+  const unsigned long interruptTime = millis();
 
-  if (interrupt_time - last_interrupt_time > 200) {
+  if (interruptTime - lastInterruptTime > 200) {
     if (digitalRead(button) == HIGH) {
-      isPressed = !isPressed;
+      ledState = !ledState;
     }
-    if (isPressed == true) {
+    if (ledState == true) {
       digitalWrite(display_LED, HIGH);
     } else {
       digitalWrite(display_LED, LOW);
     }
-    last_interrupt_time = interrupt_time;
+    lastInterruptTime = interruptTime;
   }
 }
 
